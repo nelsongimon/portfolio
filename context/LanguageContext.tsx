a7f0d06d9bf02995f0eefc4cbb281e69c5348bab
Nelson Gimon <nelson.gimon@gmail.com>
2023-08-29 19:51:00 -0400

    Add local storage to language switcher

--- context/LanguageContext.tsx
+++ context/LanguageContext.tsx
@@ -18,21 +18,17 @@ export default function LanguageProvider({
   children
 }: { children: React.ReactNode }) {
 
-
-  const initialLanguage = localStorage.getItem("selectedLanguage") || "es";
-
-  const [language, setLanguage] = useState<AvailableLanguages>(initialLanguage as AvailableLanguages);
-  // const [language, setLanguage] = useState<AvailableLanguages>("es");
+  const [language, setLanguage] = useState<AvailableLanguages>("es");
 
   useEffect(() => {
-    // if (typeof window !== "undefined") {
-    //   const selectedLanguage = localStorage.getItem("selectedLanguage");
-    //   if (selectedLanguage) {
-    //     setLanguage(selectedLanguage as AvailableLanguages);
-    //   } else {
-    //     localStorage.setItem("selectedLanguage", "es");
-    //   }
-    // }
+    if (typeof window !== "undefined") {
+      const selectedLanguage = localStorage.getItem("selectedLanguage");
+      if (selectedLanguage) {
+        setLanguage(selectedLanguage as AvailableLanguages);
+      } else {
+        localStorage.setItem("selectedLanguage", "es");
+      }
+    }
   }, []);
 
   const changeLanguage = (language: AvailableLanguages) => {
